9774b2c1d96872e16e9d38a1268e84de4b258e15
MinaSamirSaad <minasamir.nashid@gmail.com>
2025-04-02 20:20:13 +0200

    Add support for ES modules and update project name in package.json

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import fs from 'fs-extra';
 import path from 'path';
 import { Command } from 'commander';
 import chalk from 'chalk';
+import { fileURLToPath } from 'url';
 
 const program = new Command();
 program
@@ -11,6 +12,8 @@ program
     .description('CLI to set up a TypeScript + Express project')
     .argument('<project-name>', 'Name of the project')
     .action((projectName) => {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
         const projectPath = path.join(process.cwd(), projectName);
         console.log(chalk.green(`Creating project at ${projectPath}...`));
 
